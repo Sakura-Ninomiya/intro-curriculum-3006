8b035958ea2274cafb4b0b93c5e164750806f98d
Sakura-Ninomiya <sakuranbo.ykmt@gmail.com>
2022-12-13 22:30:50 +0900

    おみくじ機能を追加

--- bolt.js
+++ bolt.js
@@ -14,4 +14,10 @@ app.message(/hello/i, ({message, say}) => {
   say('こんにちは！');
 });
 
+app.message(/おみくじ/i, ({message, say}) => {
+  let items = ["大吉", "中吉", "吉", "小吉", "凶"];
+  let lot = lots[Math.floor(Math.random() * items.length)];
+  say( `${lot}, <@${message.user}>`);
+});
+
 app.start();
